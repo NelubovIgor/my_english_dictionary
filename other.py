448b9abf5f11bd943991c23d050a877b5b6d5b59
Igor <nelubov.igor@gmail.com>
2025-01-30 18:04:33 +0300

    void commit1

--- other.py
+++ other.py
@@ -1,7 +1,18 @@
 from collections import defaultdict
+import asyncio
+
+async def double(x):
+    return x * 2
+
+async def main():
+    tasks = [double(i) for i in range(3)]
+    r = await asyncio.gather(*tasks)
+    print(r)
+
+asyncio.run(main())
 
 d = defaultdict(list)
-print(d)
+
 keys = [1, 1, 2, 3, 2]
 
 for k in keys:
