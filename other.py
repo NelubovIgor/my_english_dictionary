ad0f6c589ff218265dab7498d071d44294d45804
Igor <nelubov.igor@gmail.com>
2025-01-31 09:31:54 +0300

    test2

--- other.py
+++ other.py
@@ -1,12 +1,62 @@
 from collections import defaultdict
 import asyncio
 from operator import itemgetter
+from itertools import groupby
+from dataclasses import dataclass, field
+from functools import lru_cache, partial
 
-data = [(1, "c"), (2, "b"), (1, "a")]
+def foo(x=[]):
+    x.append(1)
+    return len(x)
 
-data.sort(key=itemgetter(0, 1))
-print(data)
-print([x[1] for x in data])
+print(foo())
+print(foo())
+print(foo([]))
+
+def add(x, y=0, z=0): return x + y + z
+f1 = partial(add, 1)
+f2 = partial(f1, y = 2)
+# print(f"{f1(2)}, {f2(z=3)}")
+
+class Descriptor:
+    def __get__(self, obj, type=None):
+        return 42
+    
+class A:
+    x = Descriptor()
+    def __getattr__(self, name):
+        return 21
+    
+a = A()
+# print(f"{a.x}, {a.y}")
+
+@lru_cache
+def fib(n):
+    if n < 2: return n
+    return fib(n-1) + fib(n-2)
+
+r = [fib(i) for i in [3, 3, 4]]
+# print(r)
+
+@dataclass
+class Box:
+    items: list = field(default_factory=list)
+    size: int = 0
+
+b1 = Box()    
+b2 = Box()
+b1.items.append(1)
+# print(f"{b1.items}, {b2.items}")
+
+data = [1, 1, 2, 3, 3, 3, 2]
+res = []
+for k, g in groupby(sorted(data)):
+    res.append(len(list(g)))
+# print(res)
+
+data1 = [(1, "c"), (2, "b"), (1, "a")]
+data1.sort(key=itemgetter(0, 1))
+# print([x[1] for x in data1])
 
 async def double(x):
     return x * 2
