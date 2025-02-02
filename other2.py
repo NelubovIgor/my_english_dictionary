b32d69eec14cf1f95e2d35af15ba549833d966d8
Igor <nelubov.igor@gmail.com>
2025-02-02 09:04:30 +0300

    test5

---
+++ other2.py
@@ -0,0 +1,13 @@
+from typing import Any
+from itertools import zip_longest
+
+a = [1, 2]
+b = [3, 4, 5]
+
+def safe_int(x: Any) -> tuple[bool, int]:
+    try:
+        return True, int(x)
+    except:
+        return False, 0
+    
+# print([safe_int(x) for x in ['1', 'a', '2']])
